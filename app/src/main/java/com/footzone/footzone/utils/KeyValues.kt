cb9060cba452e58ef24ccfcd1bd3b18f79e97f84
Marufjon <pardayevmaruf80@gmail.com>
2022-05-24 23:51:55 +0500

    location find first

--- app/src/main/java/com/footzone/footzone/utils/KeyValues.kt
+++ app/src/main/java/com/footzone/footzone/utils/KeyValues.kt
@@ -1,5 +1,10 @@
 package com.footzone.footzone.utils
 
+import android.location.Location
+
 object KeyValues {
     const val PITCH_DETAIL = "pitchDetail"
+
+    //current location variable
+    var location: Location? = null
 }
