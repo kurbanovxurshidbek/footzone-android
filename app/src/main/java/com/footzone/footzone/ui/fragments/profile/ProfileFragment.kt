7303972df45de1e12315e9146b77d49df25cd8ca
HP <rodilbek@gmail.com>
2022-06-05 11:38:20 +0300

    all ViewModel

--- app/src/main/java/com/footzone/footzone/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/footzone/footzone/ui/fragments/profile/ProfileFragment.kt
@@ -106,9 +106,11 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
     private fun showUserData(userData: UserData) {
         binding.tvName.text = userData.data.fullName
         binding.tvNumber.text = userData.data.phoneNumber
-//        Glide.with(requireActivity())
-//            .load("http://10.10.2.18:8081/images/${userData.data.photo.name}")
-//            .into(binding.ivProfile)
+        Glide.with(requireActivity())
+            .load("http://10.10.2.18:8081/images/${userData.data.photo.name}")
+            .into(binding.ivProfile)
+
+        Log.d("TAG", "showUserData: ${"http://10.10.2.18:8081/images/${userData.data.photo.name}"}")
     }
 
     private fun showPopup(v: View) {
