6a0c4a76378c1d4f176643dec95787e0381b321b
Perennial2002 <pendragon05082002@gmail.com>
2022-06-19 05:34:48 +0500

    map completed

--- app/src/main/java/com/footzone/footzone/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/footzone/footzone/ui/activity/BaseActivity.kt
@@ -1,11 +1,18 @@
 package com.footzone.footzone.ui.activity
 
 import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.PermissionChecker
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -25,7 +32,7 @@ open class BaseActivity : AppCompatActivity() {
     fun permissionRequest() {
         if (isLocationPermissionGranted()
         ) {
-            openMainActivity()
+            showLocationOn()
         } else {
             request()
         }
@@ -52,7 +59,7 @@ open class BaseActivity : AppCompatActivity() {
             }).check()
     }
 
-    private fun openMainActivity() {
+    open fun openMainActivity() {
         startActivity(Intent(context, MainActivity::class.java))
     }
 
@@ -61,4 +68,45 @@ open class BaseActivity : AppCompatActivity() {
             context!!,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PermissionChecker.PERMISSION_GRANTED
+
+    private fun showLocationOn() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 30 * 1000.toLong()
+            fastestInterval = 5 * 1000.toLong()
+        }
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        builder.setAlwaysShow(true)
+        val result =
+            LocationServices.getSettingsClient(context!!)
+                .checkLocationSettings(builder.build())
+        result.addOnCompleteListener {
+            try {
+                val response: LocationSettingsResponse = it.getResult(ApiException::class.java)
+                if (response.locationSettingsStates!!.isGpsPresent){
+                    openMainActivity()
+                }
+            } catch (e: ApiException) {
+                when (e.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                        val intentSenderRequest =
+                            IntentSenderRequest.Builder(e.status.resolution!!).build()
+                        launcher.launch(intentSenderRequest)
+                    } catch (e: IntentSender.SendIntentException) {
+                    }
+                }
+            }
+        }
+    }
+
+    private var launcher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                openMainActivity()
+            } else {
+                showLocationOn()
+            }
+        }
 }
--- app/src/main/java/com/footzone/footzone/ui/fragments/location/StadiumLocationFragment.kt
+++ app/src/main/java/com/footzone/footzone/ui/fragments/location/StadiumLocationFragment.kt
@@ -1,157 +1,134 @@
 package com.footzone.footzone.ui.fragments.location
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import com.footzone.footzone.R
 import com.footzone.footzone.databinding.FragmentStadiumLocationBinding
 import com.footzone.footzone.ui.fragments.BaseFragment
 import com.footzone.footzone.utils.KeyValues
-import com.footzone.footzone.utils.LocationHelper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 
 
-class StadiumLocationFragment : BaseFragment(R.layout.fragment_stadium_location),
-    OnMapReadyCallback,
-    GoogleMap.CancelableCallback, GoogleMap.OnMarkerDragListener {
+class StadiumLocationFragment : BaseFragment(R.layout.fragment_stadium_location) {
 
     lateinit var binding: FragmentStadiumLocationBinding
-    private var lastLocation: Location? = null
-    private var cameraCurrentLatLng: LatLng? = null
+    private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var mMap: GoogleMap
-    private val myLocationZoom = 16.0f
+    private var lastLocation: Location? = null
+    private val myLocationZoom = 10f
+    private var cameraCurrentLatLng: LatLng? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initFusedLocationClient()
-    }
-
-    private fun initFusedLocationClient() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentStadiumLocationBinding.bind(view)
         initViews()
+        installLocation()
     }
 
+    private val callback = OnMapReadyCallback { googleMap ->
+        //  googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        googleMap.uiSettings.isZoomControlsEnabled = false
+        googleMap.isMyLocationEnabled = true
+        googleMap.uiSettings.isMyLocationButtonEnabled = false
+        map = googleMap
+        cameraMoveStartedListener(googleMap)
+        setUpMap()
+    }
 
     private fun initViews() {
-        val locationCur = setUpMap()
         binding.apply {
             icClose.setOnClickListener { requireActivity().onBackPressed() }
             tvCancel.setOnClickListener { requireActivity().onBackPressed() }
             tvSelection.setOnClickListener {
-                Log.d("TAG", "initViews: ${locationCur.latitude} ${locationCur.longitude} ")
                 setFragmentResult(
                     KeyValues.TYPE_LOCATION,
                     bundleOf(
-                        "latitude" to locationCur.latitude,
-                        "longitude" to locationCur.longitude
+                        "latitude" to cameraCurrentLatLng!!.latitude,
+                        "longitude" to cameraCurrentLatLng!!.longitude
                     )
                 )
                 findNavController().popBackStack()
             }
         }
-
-        val supportMapFragment =
-            childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment
-        supportMapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        setUpMap()
-        mMap = p0
-        mMap.moveCamera(
-            CameraUpdateFactory.newLatLngZoom(
-                LatLng(41.2795325, 69.2143852),
-                myLocationZoom
-            )
-        )
-        val target = LatLng(41.2795325, 69.2143852)
-        val cameraUpdate = CameraPosition.Builder().target(target).zoom(myLocationZoom).build()
-        mMap.moveCamera(
-            CameraUpdateFactory.newCameraPosition(cameraUpdate)
-        )
+    private fun installLocation() {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
 
-        mMap.setOnCameraMoveStartedListener {
-            //todo start animate marker
-            binding.mapIcon.animate().setDuration(300).translationY(-binding.mapIcon.height / 2.0f)
-                .start()
+    private fun cameraMoveStartedListener(googleMap: GoogleMap) {
+        googleMap.setOnCameraMoveStartedListener {
+            binding.mapIcon.animate()
         }
-
-        mMap.setOnCameraIdleListener {
-            binding.mapIcon.animate().translationY(0f).setDuration(300).start()
-
+        googleMap.setOnCameraIdleListener {
+            cameraCurrentLatLng = googleMap.cameraPosition.target
         }
     }
 
-    private fun setUpMap(): LatLng {
-        var currentLatLng: LatLng? = null
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+    private fun setUpMap() {
+        setupMe()
+        btnMyLocationClickManager()
+    }
 
-        }
-        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+    private fun setupMe() {
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
             if (location != null) {
                 lastLocation = location
-                currentLatLng = LatLng(location.latitude, location.longitude)
-                mMap.animateCamera(
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(
                     CameraUpdateFactory.newLatLngZoom(
-                        currentLatLng!!,
+                        currentLatLng,
                         myLocationZoom
                     )
                 )
+            } else {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    fusedLocationClient =
+                        LocationServices.getFusedLocationProviderClient(requireContext())
+                    toast("ERROR loading location")
+                }, 400)
             }
         }
-        return currentLatLng!!
-
+        fusedLocationClient.lastLocation.addOnFailureListener {
+            toast("$it")
+        }
     }
 
-
-    override fun onCancel() {
-
+    private fun btnMyLocationClickManager() {
+        binding.findMyLocation.setOnClickListener {
+            updateLastLocation()
+        }
     }
 
-    override fun onFinish() {
-
+    private fun updateLastLocation() {
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    lastLocation!!.latitude,
+                    lastLocation!!.longitude
+                ), myLocationZoom
+            )
+        )
     }
 
-    override fun onMarkerDrag(p0: Marker) {
-
-    }
-
-    override fun onMarkerDragEnd(p0: Marker) {
-        Log.d("TAG", "onMarkerDragEnd: ${p0.position.latitude} ${p0.position.longitude}")
-    }
-
-    override fun onMarkerDragStart(p0: Marker) {
-
-    }
 }
