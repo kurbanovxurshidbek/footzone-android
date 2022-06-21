64833143af9f5c188cb20afb45a9598822c2e94d
HP <rodilbek@gmail.com>
2022-06-21 10:45:41 +0300

    booking optimization

--- app/src/main/java/com/footzone/footzone/ui/fragments/addstadium/AddStadiumFragment.kt
+++ app/src/main/java/com/footzone/footzone/ui/fragments/addstadium/AddStadiumFragment.kt
@@ -2,17 +2,21 @@ package com.footzone.footzone.ui.fragments.addstadium
 
 import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.provider.OpenableColumns
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.setFragmentResultListener
@@ -37,11 +41,15 @@ import com.footzone.footzone.utils.KeyValues.WORK_TIMES
 import com.footzone.footzone.utils.SharedPref
 import com.footzone.footzone.utils.UiStateObject
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import me.shouheng.compress.strategy.compress.Compressor
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
 import java.util.*
 import javax.inject.Inject
 
@@ -500,7 +508,7 @@ open class AddStadiumFragment : BaseFragment(R.layout.fragment_add_stadium) {
      * This is function, to upgrade from Uri to MultipartBody.Part
      */
     private fun convertUriMultipart(selectedImageUri: Uri, name: String): MultipartBody.Part {
-        val ins = requireActivity().contentResolver.openInputStream(selectedImageUri)
+        val ins = requireActivity().contentResolver.openInputStream(getFilePathFromUri(selectedImageUri, requireContext()))
         val image = File.createTempFile(
             "file", ".jpg",
             requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
@@ -515,6 +523,59 @@ open class AddStadiumFragment : BaseFragment(R.layout.fragment_add_stadium) {
             MultipartBody.Part.createFormData(name, image.name, reqFile)
 
         return body
+    }
+
+    @Throws(IOException::class)
+    fun getFilePathFromUri(uri: Uri, context: Context): Uri {
+        val fileName: String? = getFileName(uri, context)
+        val file = File(context?.externalCacheDir, fileName)
+        file.createNewFile()
+        FileOutputStream(file).use { outputStream ->
+            context?.contentResolver?.openInputStream(uri).use { inputStream ->
+                copyFile(inputStream, outputStream)
+                outputStream.flush()
+            }
+        }
+        return Uri.fromFile(file)
+    }
+
+    @Throws(IOException::class)
+    private fun copyFile(`in`: InputStream?, out: OutputStream) {
+        val buffer = ByteArray(1024)
+        var read: Int? = null
+        while (`in`?.read(buffer).also({ read = it!! }) != -1) {
+            read?.let { out.write(buffer, 0, it) }
+        }
+    }//copyFile ends
+
+    fun getFileName(uri: Uri, context: Context): String {
+        var fileName: String? = getFileNameFromCursor(uri, context)
+        if (fileName == null) {
+            val fileExtension: String? = getFileExtension(uri, context)
+            fileName = "temp_file" + if (fileExtension != null) ".$fileExtension" else ""
+        } else if (!fileName.contains(".")) {
+            val fileExtension: String? = getFileExtension(uri, context)
+            fileName = "$fileName.$fileExtension"
+        }
+        return fileName
+    }
+
+    fun getFileExtension(uri: Uri, context: Context?): String? {
+        val fileType: String? = context?.contentResolver?.getType(uri)
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(fileType)
+    }
+
+    fun getFileNameFromCursor(uri: Uri, context: Context?): String? {
+        val fileCursor: Cursor? = context?.contentResolver
+            ?.query(uri, arrayOf<String>(OpenableColumns.DISPLAY_NAME), null, null, null)
+        var fileName: String? = null
+        if (fileCursor != null && fileCursor.moveToFirst()) {
+            val cIndex: Int = fileCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (cIndex != -1) {
+                fileName = fileCursor.getString(cIndex)
+            }
+        }
+        return fileName
     }
 
     private fun openStadiumLocation() {
