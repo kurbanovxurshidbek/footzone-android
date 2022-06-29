ecffef2a8befd389d68a75537a1e4e64dfde0a76
Perennial2002 <pendragon05082002@gmail.com>
2022-06-29 17:18:31 +0500

    some changes

--- app/src/main/java/com/footzone/footzone/adapter/AdminNotificationAdapter.kt
+++ app/src/main/java/com/footzone/footzone/adapter/AdminNotificationAdapter.kt
@@ -1,13 +1,18 @@
 package com.footzone.footzone.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.footzone.footzone.R
 import com.footzone.footzone.databinding.ItemAdminNotificationBinding
 import com.footzone.footzone.helper.OnClickEventAcceptDecline
 import com.footzone.footzone.model.AdminNotification
 import com.footzone.footzone.model.StadiumBookSentResponseData
+import com.footzone.footzone.utils.commonfunction.Functions
 import com.footzone.footzone.utils.extensions.hide
+import java.time.LocalTime
 
 class AdminNotificationAdapter(
     var notifications: List<StadiumBookSentResponseData>,
@@ -26,9 +31,32 @@ class AdminNotificationAdapter(
         )
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: VH, position: Int) {
         val notification = notifications[position]
+
+        val duration = Functions.calculateInHours(
+            LocalTime.parse(notification.startTime),
+            LocalTime.parse(notification.endTime)
+        )
         holder.view.apply {
+            tvStadiumName.text =
+                "${notification.stadiumName} ${tvStadiumName.context.getText(R.string.str_football_stadium)}"
+            tvDate.text = notification.startDate
+            tvHours.text =
+                "${notification.startTime.subSequence(0, 5)}-${notification.endTime.substring(0, 5)}, " +
+                        "$duration ${tvHours.context.getText(R.string.str_hour)}"
+            tvPrice.text = "${(notification.hourlyPrice.toInt() * duration).toInt()} so'm"
+            "${notification.startTime.subSequence(0, 5)}-${
+                notification.endTime.substring(
+                    0,
+                    5
+                )
+            }, $duration soat"
+            tvPrice.text = "${notification.hourlyPrice.toInt() * duration} so'm"
+        }
+
+            holder.view.apply {
             btnAccept.setOnClickListener {
                 linearButtonWrapper.hide()
                 onClickEventAcceptDecline.onAccept(notification.id, tvAccepted, acceptedLayout)
